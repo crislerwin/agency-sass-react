e90cc6a2ebf9213db9879ed75c27fa6c512a830b
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-19 06:20:17 -0300

    ✅ create a services component

---
+++ src/components/Services.js
@@ -0,0 +1,74 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export const Services = () => {
+  return (
+    <>
+      <div className="services-container">
+        <h2>O que fazemos</h2>
+        <div className="services-card-container">
+          <div className="services-card">
+            <i className="fab fa-buffer"></i>
+            <h3>Desenvolvimento FullStack</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Assumenda eos odio velit enim expedita culpa neque inventore
+              eveniet! Ex similique ut pariatur eveniet et inventore laborum
+              deserunt optio ab officiis!
+            </p>
+          </div>
+          <div className="services-card">
+            <i className="fas fa-bolt"></i>
+            <h3>Sites leves e rápidos</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              numquam molestiae quisquam quidem? Accusantium repudiandae, velit
+              asperiores voluptatum nostrum tenetur aliquam doloremque rem.
+              Sapiente, cumque voluptatem maxime iusto quidem veritatis?
+            </p>
+          </div>
+          <div className="services-card">
+            <i className="fas fa-cart-plus"></i>
+            <h3>E-commerce</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium velit soluta, sequi excepturi amet a. Mollitia, at
+              maxime nisi molestias beatae dolores dolorem cupiditate
+              reprehenderit fugit modi animi, perspiciatis doloremque!
+            </p>
+          </div>
+          <div className="services-card">
+            <i className="fas fa-chart-line"></i>
+            <h3>SEO</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea a
+              pariatur expedita neque doloremque odio eius maiores. Autem eaque
+              consequatur voluptates ipsam voluptatem quos facere temporibus,
+              nobis, corporis vero eveniet?
+            </p>
+          </div>
+          <div className="services-card">
+            <i className="fas fa-chess-pawn"></i>
+            <h3>Estratégias de marketing</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consequuntur reiciendis quas animi dolorem labore consequatur
+              minima eaque deleniti corrupti minus. Architecto eos assumenda
+              cum, praesentium repellat adipisci officiis itaque placeat!
+            </p>
+          </div>
+          <div className="services-card">
+            <i className="fas fa-cloud"></i>
+            <h3>Hospedagem AWS/Azure</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              animi iure quo nihil sunt odio nobis dolore minima fugit ut, autem
+              ex, vel aliquid. Et labore ducimus repudiandae eos at!
+            </p>
+          </div>
+        </div>
+        <a href="#" className="btn btn-primary btn rounded">
+          Veja todos os serviços
+        </a>
+      </div>
+    </>
+  );
+};
