1d2afc96764d709b2bdea637483d27b58c76e899
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-19 06:18:06 -0300

    ✅ create a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="footer-container">
+          <div className="footer-desc-container">
+            <h3 className="footer-title">Agency</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+              architecto aperiam itaque consectetur repudiandae? Ipsa eveniet
+              atque nisi laudantium cupiditate dignissimos quis et blanditiis
+              modi, earum similique sapiente repudiandae voluptas.
+            </p>
+          </div>
+          <div className="footer-links-container">
+            <div className="footer-links-container-list">
+              <p className="footer-title">Links</p>
+              <ul>
+                <li>
+                  <a href="#">Base de conhecimentos</a>
+                </li>
+                <li>
+                  <a href="#">Trabalhe conosco</a>
+                </li>
+                <li>
+                  <a href="#">Últimos Projetos</a>
+                </li>
+                <li>
+                  <a href="#">Política de Privacidade</a>
+                </li>
+                <li>
+                  <a href="#">Contato</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-links-container-list">
+              <p className="footer-title">Links</p>
+              <ul>
+                <li>
+                  <a href="#">Base de conhecimentos</a>
+                </li>
+                <li>
+                  <a href="#">Trabalhe conosco</a>
+                </li>
+                <li>
+                  <a href="#">Últimos Projetos</a>
+                </li>
+                <li>
+                  <a href="#">Política de Privacidade</a>
+                </li>
+                <li>
+                  <a href="#">Contato</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-copy-right-container">
+            <p>&copy; 2021 Agency</p>
+            <p>A melhor agencia web para seus projetos</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
