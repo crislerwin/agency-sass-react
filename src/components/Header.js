9e080cec9d8333382b708d09fd7e10db44e33ea3
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-18 21:46:43 -0300

    create a component Header

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+const Header = () => {
+  return (
+    <header className="main-banner">
+      <div className="nav-container">
+        <a href="#" className="brand">
+          <i className="fas fa-brain"></i>
+        </a>
+        <nav>
+          <ul className="nav-container-navbar">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+            <li>
+              <a href="#" className="btn btn-primary">
+                Entrar
+              </a>
+            </li>
+            <li>
+              <a href="#" className="btn">
+                Registrar
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <i className="fas fa-bars"></i>
+      </div>
+      <div className="title-container">
+        <h2>Nós desenvolvemos</h2>
+        <h1>Poderosas Experiências</h1>
+        <a href="#" className="btn btn-primary btn-rounded">
+          Fazer Orçamento
+        </a>
+      </div>
+    </header>
+  );
+};
+export default Header;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,3 +1,9 @@
+import Header from "../components/Header";
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
