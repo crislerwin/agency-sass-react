34501a764c605a4b63e54d96a0491cf48b2cd9a7
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-19 06:20:36 -0300

    ✅ import components

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,14 @@
-import Header from "../components/Header";
-
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Jobs } from "../components/Jobs";
+import { Services } from "../components/Services";
 export default function App() {
   return (
     <>
       <Header />
+      <Services />
+      <Jobs />
+      <Footer />
     </>
   );
 }
