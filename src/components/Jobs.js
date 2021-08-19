8105f1d9dae7c456203a68cf5c5d02044cd40c96
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-19 06:19:52 -0300

    ✅ create a jobs component

---
+++ src/components/Jobs.js
@@ -0,0 +1,57 @@
+export const Jobs = () => {
+  return (
+    <>
+      <div className="jobs-container">
+        <h2>Alguns de nossos trabalhos</h2>
+        <div className="jobs-list">
+          <div className="first-job" id="job1">
+            <div className="job-card-cover">
+              <p className="job-card-title">Projeto 1</p>
+              <p className="job-card-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+                aperiam distinctio labore praesentium sunt reprehenderit
+                perferendis impedit rem quibusdam et aliquid ratione asperiores,
+                fugiat, commodi architecto non doloribus, sint temporibus?
+              </p>
+            </div>
+          </div>
+          <div className="others-jobs-container">
+            <div className="job-card" id="job2">
+              <div className="job-card-cover">
+                <p className="job-card-title">Projeto 2</p>
+                <p className="job-card-description">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel
+                  quod optio sunt non eum odio minus, totam, atque corrupti et
+                  aperiam soluta voluptatibus eveniet ratione accusantium
+                  doloremque illo delectus adipisci.
+                </p>
+              </div>
+            </div>
+            <div className="job-card" id="job4">
+              <div className="job-card-cover">
+                <p className="job-card-title">Projeto 4</p>
+                <p className="job-card-description">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  explicabo sed libero harum eum officiis, vero qui, aspernatur
+                  expedita facere necessitatibus consectetur magnam doloribus
+                  odio beatae quisquam laborum maxime obcaecati.
+                </p>
+              </div>
+            </div>
+            <div className="job-card" id="job5">
+              <div className="job-card-cover">
+                <p className="job-card-title">Projeto 5</p>
+                <p className="job-card-description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+                  ipsa beatae earum dolorem labore modi eum in odit, vel
+                  repellendus dicta facilis! Aliquam neque dolore consectetur
+                  sequi commodi mollitia ab.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
